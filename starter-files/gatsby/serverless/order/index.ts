5fefeb641028d5bb6773895b0405a5692dc22328
Russ Watson <russ@bakerstreet.engineering>
2021-04-23 15:56:04 +0200

    Finishes 11#39

---
+++ starter-files/gatsby/serverless/order/index.ts
@@ -0,0 +1,26 @@
+import { Handler } from "@netlify/functions";
+import NodeMailer from "nodemailer";
+
+const transporter = NodeMailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PSS,
+  },
+});
+
+const handler: Handler = async (event, context) => {
+  const info = await transporter.sendMail({
+    from: "Slick's Slices <slick@example.com>",
+    to: "orders@example.com",
+    subject: "New Order!",
+    html: `<p>Your new pizza order is here!</p>`,
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  };
+};
+
+export { handler };
